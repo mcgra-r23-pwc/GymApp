71fbc411aa505760b88672e776c6abcbac3ca35b
Rory McGrath <rory.m.mcgrath@pwc.com>
2022-10-11 13:51:47 +0100

    Refactored Push
    Bit of refactoring done on JetBrains Rider.

--- Program.cs
+++ Program.cs
@@ -10,53 +10,52 @@ namespace GymApp
             Console.WriteLine("Hi!\n");
             Console.WriteLine("\tExisting member?\tLogin (L)\n\n\tNew member?\t\tRegister (R)\n\n\t\t\t\tExit (X)\n");
 
-            char loginRegister = char.ToUpper(Console.ReadKey().KeyChar);
+            var loginRegister = char.ToUpper(Console.ReadKey().KeyChar);
             if (loginRegister == 'L')
             {
                 // set up list of members
-                List<Member> memberList = new List<Member>();
-
-                memberList.Add(new Member()
+                var memberList = new List<Member>
                 {
-                    FirstName = "Rory",
-                    LastName = "McGrath",
-                    MembershipNumber = 040118,
-                    MemberType = "Personal Trainer",
-                    RenewalDate = new DateTime(2023, 01, 17),
-                    HouseNumber = "123",
-                    StreetName = "This Place",
-                    TownCity = "Carryduff",
-                    County = "Down",
-                    PostCode = "BT8 7RS"
-                });
-
-                memberList.Add(new Member()
-                {
-                    FirstName = "Arthur",
-                    LastName = "Guinness",
-                    MembershipNumber = 231109,
-                    MemberType = "Gym User",
-                    RenewalDate = new DateTime(2023, 03, 17),
-                    HouseNumber = "456",
-                    StreetName = "That Place",
-                    TownCity = "Belfast",
-                    County = "Antrim",
-                    PostCode = "BT9 7HQ"
-                });
-
-                memberList.Add(new Member()
-                {
-                    FirstName = "David",
-                    LastName = "Clifford",
-                    MembershipNumber = 241222,
-                    MemberType = "Gym User",
-                    RenewalDate = new DateTime(2023, 09, 11),
-                    HouseNumber = "789",
-                    StreetName = "The Other Place",
-                    TownCity = "Derrymacash",
-                    County = "Armagh",
-                    PostCode = "BT66 6LG"
-                });
+                    new Member()
+                    {
+                        FirstName = "Rory",
+                        LastName = "McGrath",
+                        MembershipNumber = 040118,
+                        MemberType = "Personal Trainer",
+                        RenewalDate = new DateTime(2023, 01, 17),
+                        HouseNumber = "123",
+                        StreetName = "This Place",
+                        TownCity = "Carryduff",
+                        County = "Down",
+                        PostCode = "BT8 7RS"
+                    },
+                    new Member()
+                    {
+                        FirstName = "Arthur",
+                        LastName = "Guinness",
+                        MembershipNumber = 231109,
+                        MemberType = "Gym User",
+                        RenewalDate = new DateTime(2023, 03, 17),
+                        HouseNumber = "456",
+                        StreetName = "That Place",
+                        TownCity = "Belfast",
+                        County = "Antrim",
+                        PostCode = "BT9 7HQ"
+                    },
+                    new Member()
+                    {
+                        FirstName = "David",
+                        LastName = "Clifford",
+                        MembershipNumber = 241222,
+                        MemberType = "Gym User",
+                        RenewalDate = new DateTime(2023, 09, 11),
+                        HouseNumber = "789",
+                        StreetName = "The Other Place",
+                        TownCity = "Derrymacash",
+                        County = "Armagh",
+                        PostCode = "BT66 6LG"
+                    }
+                };
 
                 // select a member
                 bool membershipNumberValid = false;
@@ -66,18 +65,18 @@ namespace GymApp
 
                 do
                 {
-                    int.TryParse(Console.ReadLine(), out int membershipNumber);
+                    int.TryParse(Console.ReadLine(), out var membershipNumber);
 
                     if (membershipNumber <= 0)
                     {
                         Console.WriteLine("\n\tMembership number must be in numeric format only.");
                     }
 
-                    foreach (Member c in memberList)
+                    foreach (Member m in memberList)
                     {
-                        if (membershipNumber == c.MembershipNumber)
+                        if (membershipNumber == m.MembershipNumber)
                         {
-                            selectedMember = c;
+                            selectedMember = m;
                             membershipNumberValid = true;
                         }
                     }
@@ -109,37 +108,33 @@ namespace GymApp
                     Console.WriteLine("\n\tWhich service would you like?");
                     Console.WriteLine("\t\tView Member Information (I)\n\t\tDays Remaining Balance (D)\n\t\tExit (X)\n");
 
-                    char service = char.ToUpper(Console.ReadKey().KeyChar);
+                    var service = char.ToUpper(Console.ReadKey().KeyChar);
 
-                    // View Member Information
-                    if (service == 'I')
+                    switch (service)
                     {
-                        Console.WriteLine("\n------------------------------------------------------------");
-                        Console.WriteLine($"\n\tName:\t\t{selectedMember.FullName()}");
-                        Console.WriteLine($"\n\tMembership No.:\t{selectedMember.MembershipNumber}");
-                        Console.WriteLine($"\n\tMember Type:\t{selectedMember.MemberType}");
-                        Console.WriteLine($"\n\tRenewal Date:\t{shortRenewalDate}");
-                        Console.WriteLine($"\n\tAddress:\t{selectedMember.FullAddress()}");
-                        Console.WriteLine("\n------------------------------------------------------------");
-                    }
-
-                    // View Days Remaining Balance
-                    else if (service == 'D')
-                    {
-                        Console.WriteLine("\n------------------------------------------------------------");
-                        Console.WriteLine($"\n\tYou have {remainingDays.TotalDays} days remaining until your next renewal.");
-                        Console.WriteLine("\n------------------------------------------------------------");
-                    }
-
-                    // Exit char
-                    else if (service == 'X')
-                    {
-                        exited = true;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("\nPlease enter a valid character");
+                        // View Member Information
+                        case 'I':
+                            Console.WriteLine("\n------------------------------------------------------------");
+                            Console.WriteLine($"\n\tName:\t\t{selectedMember.FullName()}");
+                            Console.WriteLine($"\n\tMembership No.:\t{selectedMember.MembershipNumber}");
+                            Console.WriteLine($"\n\tMember Type:\t{selectedMember.MemberType}");
+                            Console.WriteLine($"\n\tRenewal Date:\t{shortRenewalDate}");
+                            Console.WriteLine($"\n\tAddress:\t{selectedMember.FullAddress()}");
+                            Console.WriteLine("\n------------------------------------------------------------");
+                            break;
+                        // View Days Remaining Balance
+                        case 'D':
+                            Console.WriteLine("\n------------------------------------------------------------");
+                            Console.WriteLine($"\n\tYou have {remainingDays.TotalDays} days remaining until your next renewal.");
+                            Console.WriteLine("\n------------------------------------------------------------");
+                            break;
+                        // Exit char
+                        case 'X':
+                            exited = true;
+                            break;
+                        default:
+                            Console.WriteLine("\nPlease enter a valid character");
+                            break;
                     }
                 }
                 while (exited == false);
